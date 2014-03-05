f0353d77838ec6b1f5336b585736c6002a661b3f
Magnus Ihse Bursie <mag@icus.se>
2014-03-06 00:19:49 +0100

    Newly created scrapy project.

---
+++ playgrabber/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for playgrabber project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'playgrabber'
+
+SPIDER_MODULES = ['playgrabber.spiders']
+NEWSPIDER_MODULE = 'playgrabber.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'playgrabber (+http://www.yourdomain.com)'
