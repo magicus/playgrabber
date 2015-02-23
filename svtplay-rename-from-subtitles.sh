18829e537e6976d08ce688005c7b9926c5dee11c
Magnus Ihse Bursie <mag@icus.se>
2015-02-23 21:13:26 +0100

    Minor updates to svtplay-rename-from-subtitles.sh.

--- svtplay-rename-from-subtitles.sh
+++ svtplay-rename-from-subtitles.sh
@@ -11,9 +11,10 @@ BASE_SHOW_NAME="$(ls *.srt | sed -e 's/\([^.]*\).*/\1/' | head -1)"
 UPPERCASE_SHOW_NAME="$(echo $BASE_SHOW_NAME | sed 's/.*/\U&/')"
 
 if [[ ! -f title-suggestions.txt ]]; then
-  grep -H -A1 middle *.srt | grep -v -e '--' -e 'textning' -e "$UPPERCASE_SHOW_NAME" | awk -F 'srt-' '{ print $1 "srt-" substr($2,1,1) tolower(substr($2,2)) }' > title-suggestions.txt
+  grep -H -A1 middle *.srt | grep -v -e '--' -e '[tT]extning' -e "$UPPERCASE_SHOW_NAME" | awk -F 'srt-' '{ print $1 "srt-" substr($2,1,1) tolower(substr($2,2)) }' > title-suggestions.txt
   echo "Processed $NUM_SUBTITLES subtitles for $BASE_SHOW_NAME"
   echo "Please verify title-suggestions.txt and run this script again"
+  cat title-suggestions.txt
 else
   sed -e 's/\(.*\.E[0-9][0-9]\.\)\(.*\)\.srt-\(.*\)\r$/mv "\1\2.mp4" "\1\3.mp4"/' title-suggestions.txt > title-script.sh
   sed -e 's/\(.*\.E[0-9][0-9]\.\)\(.*\)\.srt-\(.*\)\r$/mv "\1\2.srt" "\1\3.srt"/' title-suggestions.txt >> title-script.sh
